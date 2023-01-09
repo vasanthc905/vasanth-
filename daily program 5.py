3570144edb47f15356984f6ba0cb45c5432f2aa5
vasanthc905 <122259136+vasanthc905@users.noreply.github.com>
2023-01-09 20:16:31 +0530

    Add files via upload

---
+++ daily program 5.py
@@ -0,0 +1,10 @@
+fresh=int(input("enter the number of fresh loaves purchased"))
+old=int(input("enter the number of old loaves purchased"))
+print=("regular price:Rs.185.00")
+fresh_amount=185.00*float(fresh)
+old_amount=(185*0.4)*float(old)
+Total=fresh_amount+old_amount
+print("amount of new loaves:Rs", fresh_amount)
+print("amount of dry old loaves:Rs",old_amount)
+print("total amount:Rs", Total)
+      
