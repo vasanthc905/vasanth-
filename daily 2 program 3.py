7136791d5b054488684f8af2a3657db2721e5fe8
vasanthc905 <122259136+vasanthc905@users.noreply.github.com>
2023-01-10 10:37:20 +0530

    Add files via upload

---
+++ daily 2 program 3.py
@@ -0,0 +1,6 @@
+countOfWords=len("hello this is ohython programming".split())
+print("count of Words in the given sentence:",countOfWords)
+
+print(len("hello this is phython programming".split()))
+
+print(len(input("enter input:").split()))
