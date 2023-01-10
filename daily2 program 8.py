d2c74605887d1b8f1eed251373ccc8ad3aca95ff
vasanthc905 <122259136+vasanthc905@users.noreply.github.com>
2023-01-10 21:54:16 +0530

    Add files via upload

---
+++ daily2 program 8.py
@@ -0,0 +1,21 @@
+def ismatch(s:str,p:str)-> bool:
+    rows,coloumns=(len(s),len(p))
+    if rows==0 and coloumns==0:
+        return True
+    if coloumns==0:
+        return False
+    dp=[[False for j in range(coloumns+1)]for i in range(rows+1)]
+    dp[0][0]=true
+    for i in range(2,columns+1):
+        if p[i-1]=='*':
+            dp[0][i]=dp[0][i-2]
+            for i in range(1,roes+1):
+                for j in range(1,coloumns+1):
+                    if s[i-1]==p[j-1] or p[j-1]=='.':
+                        dp[i][j]=dp[i-1[j-1]
+                    elif j>1 and p[j-1]=='*':
+                        dp[i][j]=dp[i][j-2]
+                        if p[j-2]=='.' or p[j-2]==s[i-1]:
+                            dp[i][j]=dp[i][j] or dp[i-1][j]
+return do[rows][coloumns]
+print(ismatch("a","aa"))
