bb1b6fcb803bf388fa94e1068e550349e3d467ed
vasanthc905 <122259136+vasanthc905@users.noreply.github.com>
2023-01-10 20:29:48 +0530

    Add files via upload

---
+++ daily 2 progarm 5.py
@@ -0,0 +1,26 @@
+def calculate(self,S):
+    def update(op,V):
+        if op=="+":stack.append(V)
+        if op=="-":stack.append(-V)
+        if op=="*":stack.append(stack.pop()*V)
+        if op=="/":stack.appen=d(int(stack.pop()/V))
+
+    it,num,stack,sign=0,0,[],"+"
+
+    while it<len(s):
+        if s[it].isdigit():
+            num=num*10+int(s[it])
+        elif s[it] in "+-*/":
+            update(sign,num)
+            num,sign=0,s[it]
+        elif s[it]=="(":
+            num,j=self.calculate(s[it+1:])
+            it=it+j
+        elif s[it]==")":
+            update(sign,num)
+            return sum(stack),it+1
+        it +=1
+    update(sign,num)
+    return sum(stack)
+        
+        
