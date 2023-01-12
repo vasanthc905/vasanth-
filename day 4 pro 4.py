4547d4d63d72ffda8996a5b54317a74fd5016563
vasanthc905 <122259136+vasanthc905@users.noreply.github.com>
2023-01-12 21:49:46 +0530

    Add files via upload

---
+++ day 4 pro 4.py
@@ -0,0 +1,18 @@
+class solution(object):
+    def ispalindrome(self,s):
+        """
+        :type s:str
+        :rtype: bool
+        """
+
+        x="""
+        diff=ord('a)-ord('a)
+        for i in s:
+            if ord(i)>=ord('a') and  ord(I)<=ord('2')or ord(i)>=ord("0")andord(i)<=ord("9"):
+               x+=i
+            elif ord(i)>=ord('a)and ord(i)<=ord('z'):
+               i=chr(diff+ord(i))
+               x+=i
+               return x==x[::-1]
+            ob1=solution()
+            print(ob1.ispalindrome("a man,a plan,a canal:panam"))
