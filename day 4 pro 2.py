c2642f9f977d558e4711c157ca902ef6f9ccbb06
vasanthc905 <122259136+vasanthc905@users.noreply.github.com>
2023-01-12 09:44:55 +0530

    Add files via upload

---
+++ day 4 pro 2.py
@@ -0,0 +1,6 @@
+total=int(input("enter a total users"))
+staff_users=int(input("enter the staff users"))
+non_teaching=staff_users/3
+student_users=total-(staff_users+non_teaching)
+n=int(student_users)
+print(n)
